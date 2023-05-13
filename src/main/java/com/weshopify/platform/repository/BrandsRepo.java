81f47d9209a39b34f35f455835aa38bac5ec4948
Narsi <nsanda@innominds.com>
2023-05-13 07:30:55 +0530

    removed the code

--- src/main/java/com/weshopify/platform/repository/BrandsRepo.java
+++
@@ -1,9 +0,0 @@
-package com.weshopify.platform.repository;
-
-import org.springframework.data.mongodb.repository.MongoRepository;
-
-import com.weshopify.platform.model.Brands;
-
-public interface BrandsRepo extends MongoRepository<Brands, Integer> {
-
-}
